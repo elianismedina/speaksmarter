bbb0a968080dcce1e43c4c367a8f48a4e391e2ea
elianismedina <elianismedina05@outlook.com>
2024-06-17 22:53:00 -0500

    changed middleware

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -36,7 +36,9 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
-            //
+            'user.roles' => $request->user() ? $request->user()->roles->pluck('name')->toArray() : [],
+            'user.permissions' => $request->user() ? $request->user()->getPermissionsViaRoles()->pluck('name')->toArray() : [],
+
         ]);
     }
 }
